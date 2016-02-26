e5f3905cf43b6ff1907578941a596c6b2936b29e
Maximilian Matthé <maximilian.matthe@ifn.et.tu-dresden.de>
2016-02-26 13:58:06 +0100

    add documentation

--- pyISCML/convolutional.py
+++ pyISCML/convolutional.py
@@ -4,6 +4,27 @@ from numpy.random import RandomState
 
 
 def Encode(bits, g, codeType):
+    """Encodes a NSC or RSC convolutional code with a tail.
+
+The calling syntax is:
+    output = Encode(input, g_encoder, code_type)
+
+Where:
+    output     : code word
+
+    input      : data word
+    g_encoder  : generator matrix for convolutional code
+                 (If RSC, then feedback polynomial is first)
+
+    code_type  : = 0 for recursive systematic convolutional (RSC) code (default)
+                 = 1 for non-systematic convolutional (NSC) code
+                 = 2 for tail-biting NSC code
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+"""
     if codeType < 2:
         outLen = g.shape[0]*(bits.shape[0] + g.shape[1]-1)
     else:
@@ -12,6 +33,36 @@ def Encode(bits, g, codeType):
     return encoded
 
 def SisoDecode(input_c, g, code_type, decoder_type):
+    """% SisoDecode performs soft-in/soft-out decodeing of a convolutional code.
+
+The calling syntax is:
+     (output_u, output_c) = SisoDecode(input_c, g_encoder, code_type, dec_type)
+
+Where:
+    output_u    : LLR of the data bits
+    output_c    : LLR of the code bits
+
+    input_c     : APP of the code bits
+    g_encoder   : generator matrix for convolutional code
+                 (If RSC, then feedback polynomial is first)
+
+    code_type   : 0 for RSC outer code (default)
+                : 1 for NSC outer code
+    dec_type    : the decoder type:
+                  = 0 For linear approximation to log-MAP (DEFAULT)
+                  = 1 For max-log-MAP algorithm (i.e. max*(x,y) = max(x,y) )
+                  = 2 For Constant-log-MAP algorithm
+                  = 3 For log-MAP, correction factor from small
+                      nonuniform table and interpolation
+                  = 4 For log-MAP, correction factor uses C function
+                      calls (slow)
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+"""
+
     input_u = np.zeros(input_c.shape[0]/g.shape[0]-g.shape[1]+1, dtype=float)
     assert code_type != 2, "Tail-biting not supported for SISO decode"
 
--- pyISCML/ldpc.py
+++ pyISCML/ldpc.py
@@ -7,6 +7,28 @@ from numpy.random import RandomState
 import _pyISCML
 
 def InitializeWiMaxLDPC(rate, codedLength):
+    """Initialies the WiMax LDPC encoder/decoder
+
+The calling syntax is:
+    (H_rows, H_cols, P) = InitializeWiMaxLDPC(rate, length)
+
+where:
+    H_rows : a M-row matrix containing the indices of the non-zero
+             rows of H, excluding the dual-diagonal portion of H.
+    H_cols : a (N-M)+z-row matrix, contaiing the indices of the
+             non-zero rows of H.
+    P      : a z times z matrix used in encoding.
+
+    rate   : the code rate. Can be either 1/2, 3/4 or 5/6
+    length : the block length of the code. Must be element of
+             arange(576, 2304+96, 96)
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+"""
     rateDict = {}
     rateDict[5./6.] = '56'
     rateDict[3./4.] = '34'
@@ -26,6 +48,28 @@ def InitializeWiMaxLDPC(rate, codedLength):
     return H_rows, H_cols, P
 
 def Encode(bits, H_rows, P):
+    """LdpcEncode incodes an LDPC codeword. Code must be an
+"eIRA-LDPC" type code, such as the one in the DVB-S2 standard, or
+WiMax standard.
+
+The calling syntax is:
+    codeword = LdpcEncode(bits, H_rows, P)
+
+where:
+    codeword = the encoded codeword
+
+    data   : a vector containing the data
+    H_rows : a M-row matrix containing the indices of the non-zero
+             rows of H, excluding the dual-diagonal part.
+    P      : z times z matrix usde to generate the first z check bits
+             for WiMax
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+"""
 
     nldpc = bits.shape[0] + H_rows.shape[0]
     encoded = np.zeros(nldpc, dtype=np.int32)
@@ -34,13 +78,57 @@ def Encode(bits, H_rows, P):
 
     return encoded
 
-def MpDecode(llr, H_rows, H_cols, max_iter, dec_type, r_scale_factor=1, q_scale_factor=1, bits=None, softOut=False):
+def MpDecode(llr, H_rows, H_cols, max_iter, dec_type,
+             r_scale_factor=1, q_scale_factor=1, bits=None,
+             softOut=False):
+    """MpDecode decodes a block code (e.g. LDPC) usin gthe message passing algorithm.
+
+The calling syntax is:
+    [output, errors] = MpDecode(input, H_rows, H_cols, max_iter,
+                                dec_type, r_scale_factor,
+                                q_scale_factor, bits, softOut)
+
+Where:
+    output     : matrix of dimension maxiter x N that has the decoded
+                 code bits for each iteration.
+    errors     : vector shownig the number of bit errors in each
+                 iteration (if bits are passed to the function)
+
+    input      : LLR denoting the decoder input
+    H_cols     : a N row matrix specifying the locations of the
+                 non-zero entries of the H matrix. The number of
+                 columns in the matrix is the column weight
+                 OR
+                 a K row matrix specifying locations of the nonzero
+                 entries in each coulmn of an extended IRA type
+    H_rows     : a N-K row matrix specifying the locations of the
+                 nonzero entries in each row of the H matrix. The
+                 number or columns in the matrix is the max row
+                 weight, unless this is for an H1 matrix, in which
+                 case the last n-k columns of the H matrix are equal
+                 to a known H2 matrix.
+    max_iter   : number of Ldpc iterations.
+    r_scale_factor :  amount to scale extrinsic output of c-nodes in
+                      min-sum decoding
+    q_scale_factor :  amount to scale extrinsic output of v-nodes in
+                      min-sum decoding (default = 1)
+    bits       : a vector containing the data bits (used for counting
+                 errors and for early halting). Default: all-zeros
+    softOut    : if true, decoder returns soft-output, i.e. LLR values
+                 instead of hard bits.
+
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+"""
+
     llrOut = np.zeros((llr.shape[0], max_iter), dtype=float)
     biterrors = np.zeros((max_iter, ), dtype=np.int32)
 
     if bits is None:
         bits = np.zeros(llr.shape, dtype=np.int32)
-
 
 
     _pyISCML.MpDecode(llr, H_rows.T.copy(), H_cols.T.copy(),
